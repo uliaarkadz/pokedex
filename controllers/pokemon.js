a231779e20b33377a3f8f6769e9e8757b037c1ce
ybuiko <juliabuiko@gmail.com>
2023-12-25 16:22:09 -0500

    cretaed starucure, routed new, edit, update, create, index

---
+++ controllers/pokemon.js
@@ -0,0 +1,36 @@
+// const express = require("express");
+// const router = express.Router();
+
+// const pokemons = require("../models/pokemon.js");
+
+// /**
+//  * INDEX ROUTE
+//  */
+
+// router.get("/", (req, res) => {
+//   res.render("./pokemon/index.ejs", { pokemons });
+// });
+
+// /**
+//  * NEW ROUTE
+//  */
+
+// /**
+//  * CREATE ROUTE
+//  */
+
+// /**
+//  * EDIT ROUTE
+//  */
+
+// /**
+//  * UPDATE ROUTE
+//  */
+
+// /**
+//  * DELETE ROUTE
+//  */
+
+// /**
+//  * SHOW ROUTE
+//  */
---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+const pokemonRouter = require("./controllers/pokemon.js");
+const pokemon = require("./models/pokemon.js");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(methodOverride("_method"));
+//app.use("/pokemon", pokemonRouter);
+// /**
+//  * INDEX ROUTE
+//  */
+app.get("/pokemon", (req, res) => {
+  res.render("./pokemon/index.ejs", { pokemon });
+});
+
+// /**
+//  * NEW ROUTE
+//  */
+app.get("/pokemon/new", (req, res) => {
+  const newId = getNewId(pokemon);
+  res.render("./pokemon/new.ejs", { pokemon, newId });
+});
+function getNewId(array) {
+  const ids = array.map((element) => {
+    return Number(element.id);
+  });
+  let nextId = Math.max.apply(null, ids);
+  nextId++;
+  return nextId;
+}
+// /**
+//  * CREATE ROUTE
+//  */
+app.post("/pokemon/", (req, res) => {
+  const body = req.body;
+  pokemon.push(body);
+  res.redirect("/pokemon");
+});
+
+// /**
+//  * EDIT ROUTE
+//  */
+app.get("/pokemon/:id/edit", (req, res) => {
+  const id = req.params.id;
+  const poke = pokemon[id];
+  res.render("./pokemon/edit.ejs", { poke, id });
+});
+
+// /**
+//  * UPDATE ROUTE
+//  */
+app.put("/pokemon/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  pokemon[id] = body;
+  res.redirect("/pokemon");
+});
+// /**
+//  * DELETE ROUTE
+//  */
+
+// /**
+//  * SHOW ROUTE
+//  */
+
+/**
+ * LISTENER
+ */
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
