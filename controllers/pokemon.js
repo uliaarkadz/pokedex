100aedf6185fb6fe246eb5c20df573f8a10c4917
ybuiko <juliabuiko@gmail.com>
2023-12-27 20:27:35 -0500

    added controllers and roouter

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,36 +1,87 @@
-// const express = require("express");
-// const router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-// const pokemons = require("../models/pokemon.js");
+const pokemon = require("../models/pokemon.js");
 
 // /**
 //  * INDEX ROUTE
 //  */
 
-// router.get("/", (req, res) => {
-//   res.render("./pokemon/index.ejs", { pokemons });
-// });
+router.get("/", (req, res) => {
+  res.render("./pokemon/index.ejs", { pokemon });
+});
 
 // /**
 //  * NEW ROUTE
 //  */
+router.get("/new", (req, res) => {
+  const newId = getNewId(pokemon);
+  res.render("./pokemon/new.ejs", { pokemon, newId });
+});
+
+//function to generate new id
+function getNewId(array) {
+  const ids = array.map((element) => {
+    return Number(element.id);
+  });
+  let nextId = Math.max.apply(null, ids);
+  nextId++;
+  return nextId;
+}
 
 // /**
 //  * CREATE ROUTE
 //  */
-
+router.post("/", (req, res) => {
+  const body = req.body;
+  const type = body.type;
+  body.type = type.split(",");
+  pokemon.push(body);
+  console.log(body);
+  res.redirect("/pokemon");
+});
 // /**
 //  * EDIT ROUTE
 //  */
+router.get("/:id/edit", (req, res) => {
+  const id = req.params.id;
+  const poke = pokemon[id];
+  res.render("./pokemon/edit.ejs", { poke, id });
+});
 
 // /**
 //  * UPDATE ROUTE
 //  */
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  const image = body.img.toLowerCase();
+
+  const newData = {
+    id: body.id,
+    name: body.name,
+    img: body.img.toLowerCase(),
+    type: body.type.split(","),
+  };
+  pokemon[id] = { ...pokemon[id], ...newData };
+  res.redirect("/pokemon");
+});
 
 // /**
 //  * DELETE ROUTE
 //  */
-
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  pokemon.splice(id, 1);
+  res.redirect("/pokemon");
+});
 // /**
 //  * SHOW ROUTE
 //  */
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const poke = pokemon[id];
+  res.render("./pokemon/show.ejs", { poke, id });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -10,74 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(methodOverride("_method"));
 app.use(express.static("public"));
-//app.use("/pokemon", pokemonRouter);
-// /**
-//  * INDEX ROUTE
-//  */
-app.get("/pokemon", (req, res) => {
-  res.render("./pokemon/index.ejs", { pokemon });
-});
-
-// /**
-//  * NEW ROUTE
-//  */
-app.get("/pokemon/new", (req, res) => {
-  const newId = getNewId(pokemon);
-  res.render("./pokemon/new.ejs", { pokemon, newId });
-});
-function getNewId(array) {
-  const ids = array.map((element) => {
-    return Number(element.id);
-  });
-  let nextId = Math.max.apply(null, ids);
-  nextId++;
-  return nextId;
-}
-// /**
-//  * CREATE ROUTE
-//  */
-app.post("/pokemon/", (req, res) => {
-  const body = req.body;
-  pokemon.push(body);
-  res.redirect("/pokemon");
-});
-
-// /**
-//  * EDIT ROUTE
-//  */
-app.get("/pokemon/:id/edit", (req, res) => {
-  const id = req.params.id;
-  const poke = pokemon[id];
-  res.render("./pokemon/edit.ejs", { poke, id });
-});
-
-// /**
-//  * UPDATE ROUTE
-//  */
-app.put("/pokemon/:id", (req, res) => {
-  const id = req.params.id;
-  const body = req.body;
-  pokemon[id] = body;
-  res.redirect("/pokemon");
-});
-// /**
-//  * DELETE ROUTE
-//  */
-
-app.delete("/pokemon/:id", (req, res) => {
-  const id = req.params.id;
-  pokemon.splice(id, 1);
-  res.redirect("/pokemon");
-});
-
-// /**
-//  * SHOW ROUTE
-//  */
-app.get("/pokemon/:id", (req, res) => {
-  const id = req.params.id;
-  const poke = pokemon[id];
-  res.render("./pokemon/show.ejs", { poke, id });
-});
+app.use("/pokemon", pokemonRouter);
 
 /**
  * LISTENER
